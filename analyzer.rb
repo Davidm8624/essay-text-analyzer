a1acbd43e561e2c90e0fce8f60234b383cf5b378
Davidm8624 <davidm8624@gmail.com>
2023-05-09 15:29:37 -0700

    character and character counter with no spaces done

--- analyzer.rb
+++ analyzer.rb
@@ -28,4 +28,12 @@ text = lines.join
 puts "#{line_count} lines"
 
 #this wasy should technacly be bettwer becaue it is simpler and uses more optimized methods to get the same thing, il need to read more of the docs to get ore stuff from it though
+#nothing wrong with the first way tho (starting on like 24)
 
+#how 2 count characters:
+
+total_characters = text.length #thsi parts works because in the last bit of code above after we counted the lines we deposited them into a single string (text) and then jsut count this part
+puts "#{total_characters} characters"
+
+total_characters_nospaces = text.gsub(/\s+/, "").length #replaces spaces with nothing and stores the new string in total...nospaces
+puts "#{total_characters_nospaces} chracters excluding the spaces"
